fbe44347a7c6e2eb987e4059f0b0dda1be78c188
Ishita Kapoor <ishitakapoor12612@gmail.com>
2022-06-19 15:46:40 +0530

    Added Splashscreen and getting started pages

--- lib/splash.dart
+++ lib/splash.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
 import 'package:e_drishti/widget.dart';
 import 'package:flutter/material.dart';
+
+import 'intro_slider.dart';
 
 class splashscreen extends StatefulWidget {
   const splashscreen({ Key? key }) : super(key: key);
@@ -9,15 +13,36 @@ class splashscreen extends StatefulWidget {
 }
 
 class _splashscreenState extends State<splashscreen> {
+
+  @override
+  void initState(){
+    super.initState();
+    Timer(Duration(seconds: 10),(){
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => IntroSliderPage()));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-      children: <Widget>[
-        appbar(title: 'E-Drishti'),
-
-      ],
-      )
+        backgroundColor: Colors.white,
+        body: Stack(
+          children: <Widget>[
+            Align( alignment: Alignment.topCenter,child:appbar(title: 'E-Drishti'),),
+            Align(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  CircleAvatar(
+                    backgroundImage: AssetImage("assets/drishti.png"),
+                    radius: MediaQuery.of(context).size.height * 0.25,
+                    backgroundColor: Colors.transparent,
+                  ),
+                ],
+              ),),
+          ],
+        )
     );
   }
 }
