49fa6803e086cb5dc75e622e9a176a09b595c620
Astha <tripathiastha369@gmail.comgit config --global user.email tripathiastha369@gmail.comgit config --global user.email tripathiastha369@gmail.com>
2022-08-21 19:26:04 +0530

    updated

--- lib/practice/practice.dart
+++ lib/practice/practice.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:e_drishti/dashboard.dart';
 
 class practice extends StatefulWidget {
   const practice({Key? key}) : super(key: key);
@@ -14,12 +14,26 @@ class _practiceState extends State<practice> {
     var size = MediaQuery.of(context).size;
     var height = size.height;
     var width = size.width;
-    return Container(
-      child: Image.asset(
-        "assets/images/comingsoon.jpg",
-        height: height/2,
-        width: width*0.8,
+    return Scaffold(
+      appBar: new AppBar(
+        title: Text('Practice'),
+        leading: new IconButton(
+          icon: new Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=> Dashboard()));
+          },
+        ),
+        centerTitle: true,
       ),
+        body: Center(
+          child: Container(
+      child: Image.asset(
+          "assets/images/comingsoon.jpg",
+          height: height/2,
+          width: width*0.8,
+      ),
+    ),
+        ),
     );
   }
 }
